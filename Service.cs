c53b7801832bb73558faddd8d9538e794ca5ef49
CTI2(IDW) Angus McLean <anguslmm@gmail.com>
2017-07-08 18:38:24 -0400

    Initialize repository

---
+++ Service.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace McRest
+{
+    class Service : IService
+    {
+
+        public Message EchoWithGet(string s)
+        {
+            return WebOperationContext.Current.CreateTextResponse(Newtonsoft.Json.JsonConvert.SerializeObject(new
+            {
+                response = "You said " + s
+            }));
+        }
+
+        public Message EchoWithPost(HelloMessage s)
+        {
+            string[] result = new string[s.Repeat];
+            string myInsult = $"You said, '{s.Greeting}{s.Message}'";
+
+            for(int i = 0; i < s.Repeat; i++)
+            {
+                result[i] = myInsult;
+            }
+            return WebOperationContext.Current.CreateTextResponse(Newtonsoft.Json.JsonConvert.SerializeObject(new
+            {
+                response = result
+            }));
+        }
+    }
+
+    [DataContract]
+    [Newtonsoft.Json.JsonObject]
+    class HelloMessage
+    {
+        [DataMember]
+        public string Message;
+
+        [DataMember]
+        public string Greeting;
+
+        [DataMember]
+        public int Repeat;
+    }
+}
