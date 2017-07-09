3b549f51358a74835b58e927b0235293835a4def
CTI2(IDW) Angus McLean <anguslmm@gmail.com>
2017-07-09 17:49:42 -0400

    Switch to Nancy

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nancy.Hosting.Self;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
@@ -13,34 +14,41 @@ namespace McRest
     {
         static void Main(string[] args)
         {
-            WebServiceHost host = new WebServiceHost(typeof(Service), new Uri("http://localhost:8000/"));
-            try
+            using (var host = new NancyHost(new Uri("http://localhost:1234")))
             {
-                ServiceEndpoint ep = host.AddServiceEndpoint(typeof(IService), new WebHttpBinding(), "");
-                host.Open();
-                using (ChannelFactory<IService> cf = new ChannelFactory<IService>(new WebHttpBinding(), "http://localhost:8000"))
-                {
-                    cf.Endpoint.Behaviors.Add(new WebHttpBehavior());
-
-                    Console.WriteLine("Endpoints are up and running.");
-                    Console.WriteLine($"They are available at '{cf.Endpoint.Address}'");
-                    cf.Endpoint.Contract.Operations.ToList().ForEach(endpoint =>
-                    {
-                        Console.WriteLine($"Endpoint: '{endpoint.Name}'");
-                    });
-                    Console.WriteLine("");
-                }
-
-                Console.WriteLine("Press <ENTER> to terminate");
+                host.Start();
+                Console.WriteLine("Running on http://localhost:1234");
                 Console.ReadLine();
+                host.Stop();
+            }
+            //WebServiceHost host = new WebServiceHost(typeof(Service), new Uri("http://localhost:8000/"));
+            //try
+            //{
+            //    ServiceEndpoint ep = host.AddServiceEndpoint(typeof(IService), new WebHttpBinding(), "");
+            //    host.Open();
+            //    using (ChannelFactory<IService> cf = new ChannelFactory<IService>(new WebHttpBinding(), "http://localhost:8000"))
+            //    {
+            //        cf.Endpoint.Behaviors.Add(new WebHttpBehavior());
 
-                host.Close();
-            }
-            catch (CommunicationException cex)
-            {
-                Console.WriteLine("An exception occurred: {0}", cex.Message);
-                host.Abort();
-            }
+            //        Console.WriteLine("Endpoints are up and running.");
+            //        Console.WriteLine($"They are available at '{cf.Endpoint.Address}'");
+            //        cf.Endpoint.Contract.Operations.ToList().ForEach(endpoint =>
+            //        {
+            //            Console.WriteLine($"Endpoint: '{endpoint.Name}'");
+            //        });
+            //        Console.WriteLine("");
+            //    }
+
+            //    Console.WriteLine("Press <ENTER> to terminate");
+            //    Console.ReadLine();
+
+            //    host.Close();
+            //}
+            //catch (CommunicationException cex)
+            //{
+            //    Console.WriteLine("An exception occurred: {0}", cex.Message);
+            //    host.Abort();
+            //}
         }
     }
 }
